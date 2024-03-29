d95bf02fe054ba631e89add30aadd2a17b30300a
Victor Reis Freitas <130703266+Vitelfs@users.noreply.github.com>
2024-03-29 15:33:32 -0300

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -27,3 +27,34 @@ function closeNav() {
     document.getElementById("content").classList.remove("content-open");
     document.getElementById("video-grid").classList.remove("video-grid-open");
 }
+
+function addCart(event){
+    
+    const product = event.target.parentNode;
+    const productName = product.querySelector('.product-name').innerText;
+    const productImage = product.querySelector('.other-item-img').src;
+    const productPrice = product.querySelector('.product-price').innerText;
+
+
+    console.log(productName); 
+    console.log(productImage); 
+    console.log(productPrice);
+
+    const productData = {
+        name: productName,
+        image: productImage,
+        price: productPrice
+    };
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+    cart.push(productData);
+  
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    if (localStorage.getItem('cart')) {
+        alert('Item adicionado ao carrinho com sucesso!');
+    } else {
+        alert('Erro ao adicionar o item ao carrinho!');
+    }
+}
